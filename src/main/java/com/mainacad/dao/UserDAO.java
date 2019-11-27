1c56dd9d85283fd25c11f430755605f00bc3a92d
Alex <ignatenko2207@gmail.com>
2019-11-27 20:34:11 +0200

    Add other DAO classes

--- src/main/java/com/mainacad/dao/UserDAO.java
+++ src/main/java/com/mainacad/dao/UserDAO.java
@@ -6,6 +6,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserDAO {
 
@@ -31,7 +33,6 @@ public class UserDAO {
 
             if( result == 1 ) {
                 ResultSet resultSet = sequenceStatement.executeQuery();
-
                 while (resultSet.next()) {
                     Integer id = resultSet.getInt(1);
                     user.setId(id);
@@ -47,15 +48,83 @@ public class UserDAO {
         return user;
     }
 
+    public static List<User> getAll() {
+        String sql = "SELECT * FROM users";
+        List<User> users = new ArrayList<>();
+        try ( Connection connection = ConnectionToDB.getConnection();
+              PreparedStatement preparedStatement =
+                      connection.prepareStatement(sql);
+              ResultSet resultSet = preparedStatement.executeQuery();
+        ) {
+            while(resultSet.next()) {
+                User user = new User (
+                        resultSet.getInt("id"),
+                        resultSet.getString("login"),
+                        resultSet.getString("password"),
+                        resultSet.getString("first_name"),
+                        resultSet.getString("last_name"),
+                        resultSet.getString("email"),
+                        resultSet.getString("phone")
+                );
+                users.add(user);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return users;
+    }
+
     public static User getById(Integer id) {
         String sql = "SELECT * FROM users WHERE id = ?";
-
+        try ( Connection connection = ConnectionToDB.getConnection();
+              PreparedStatement preparedStatement =
+                      connection.prepareStatement(sql)
+        ) {
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while(resultSet.next()) {
+                User user = new User (
+                        resultSet.getInt("id"),
+                        resultSet.getString("login"),
+                        resultSet.getString("password"),
+                        resultSet.getString("first_name"),
+                        resultSet.getString("last_name"),
+                        resultSet.getString("email"),
+                        resultSet.getString("phone")
+                );
+                return user;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
     public static User getByLoginAndPassword(String login, String password) {
         String sql = "SELECT * FROM users WHERE login = ? AND password = ?";
+        try ( Connection connection = ConnectionToDB.getConnection();
+              PreparedStatement preparedStatement =
+                      connection.prepareStatement(sql)
+              ) {
+            preparedStatement.setString(1, login);
+            preparedStatement.setString(2, password);
 
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while(resultSet.next()) {
+                User user = new User (
+                  resultSet.getInt("id"),
+                  resultSet.getString("login"),
+                  resultSet.getString("password"),
+                  resultSet.getString("first_name"),
+                  resultSet.getString("last_name"),
+                  resultSet.getString("email"),
+                  resultSet.getString("phone")
+                );
+                return user;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
@@ -68,13 +137,37 @@ public class UserDAO {
                 "e_mail = ?, " +
                 "phone = ? " +
                 "WHERE id = ?";
+        try ( Connection connection = ConnectionToDB.getConnection();
+              PreparedStatement preparedStatement =
+                      connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, user.getLogin());
+            preparedStatement.setString(2, user.getPassword());
+            preparedStatement.setString(3, user.getFirstName());
+            preparedStatement.setString(4, user.getLastName());
+            preparedStatement.setString(5, user.getEmail());
+            preparedStatement.setString(6, user.getPhone());
+            preparedStatement.setInt(6, user.getId());
 
-        return user;
+            int result = preparedStatement.executeUpdate();
+            if (result == 1) {
+                return user;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     public static void delete(Integer id) {
         String sql = "DELETE FROM users WHERE id = ?";
-
-
+        try ( Connection connection = ConnectionToDB.getConnection();
+              PreparedStatement preparedStatement =
+                      connection.prepareStatement(sql)) {
+            preparedStatement.setInt(1, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
+
 }
